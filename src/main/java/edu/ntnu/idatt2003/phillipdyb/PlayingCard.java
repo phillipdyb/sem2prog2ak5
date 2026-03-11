fbace81f533fa5783a9250df8bd974b2b7b4ea82
Phillip Dyb-Sandnes <phillipdyb@hotmail.com>
2026-03-11 18:29:34 +0100

    Add error handling

--- src/main/java/edu/ntnu/idatt2003/phillipdyb/PlayingCard.java
+++ src/main/java/edu/ntnu/idatt2003/phillipdyb/PlayingCard.java
@@ -5,8 +5,25 @@ public class PlayingCard {
   private final int value;
 
   public PlayingCard(char suit, int value) {
+    verifyValue(value);
+    verifySuit(suit);
+
     this.suit = suit;
     this.value = value;
+  }
+
+  // Error handling for value
+  public void verifyValue(int value) {
+    if (value < 1 || value > 13) {
+      throw new IllegalArgumentException("Face must be between 1 and 13");
+    }
+  }
+
+  // Error handling for char suit
+  public void verifySuit(char suit) {
+    if (suit != 'S' && suit != 'D' && suit != 'C' && suit != 'H') {
+      throw new IllegalArgumentException("Invalid character: " + suit);
+    }
   }
 
   public char getSuit() {
@@ -21,6 +38,5 @@ public class PlayingCard {
   public String toString() {
     return "" + suit + value;
   }
-
 
 }
