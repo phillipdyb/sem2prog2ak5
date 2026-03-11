8b7c31557c47e50913d9d09813730b59e1edda68
Phillip Dyb-Sandnes <phillipdyb@hotmail.com>
2026-03-11 16:04:14 +0100

    Use Random and 'n' instead of Collections to fit the task

--- src/main/java/edu/ntnu/idatt2003/phillipdyb/DeckOfCards.java
+++ src/main/java/edu/ntnu/idatt2003/phillipdyb/DeckOfCards.java
@@ -3,6 +3,7 @@ package edu.ntnu.idatt2003.phillipdyb;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class DeckOfCards {
 
@@ -20,9 +21,23 @@ public class DeckOfCards {
   }
 
   // Deals a set of 5 cards, shuffled from the collection
-  public List<PlayingCard> dealHand() {
-    Collections.shuffle(cards);
-    return cards.subList(0, 5);
+//  public List<PlayingCard> dealHand() {
+//    Collections.shuffle(cards);
+//    return cards.subList(0, 5);
+//  }
+
+  public List<PlayingCard> dealHand(int n) {
+    Random random = new Random();
+    List<PlayingCard> deck = new ArrayList<>(cards);
+    List<PlayingCard> hand = new ArrayList<>();
+
+    for (int i = 0; i < n; i++) {
+      int index = random.nextInt(deck.size());
+      hand.add(deck.get(index));
+      deck.remove(index);
+    }
+
+    return hand;
   }
 
 
