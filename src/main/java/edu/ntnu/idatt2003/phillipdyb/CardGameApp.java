a3ab9cea24edf1e437cc46e44686b2b32fdd1ed8
Phillip Dyb-Sandnes <phillipdyb@hotmail.com>
2026-03-11 15:02:17 +0100

    Add GUI elements to show cards, and get new cards

--- src/main/java/edu/ntnu/idatt2003/phillipdyb/CardGameApp.java
+++ src/main/java/edu/ntnu/idatt2003/phillipdyb/CardGameApp.java
@@ -1,13 +1,32 @@
 package edu.ntnu.idatt2003.phillipdyb;
 
 import javafx.application.Application;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class CardGameApp extends Application {
 
   @Override
   public void start(Stage primaryStage) {
+    DeckOfCards deck = new DeckOfCards();
+    HandOfCards hand = new HandOfCards(deck.dealHand());
+
+    Label label = new Label(hand.getHand().toString());
+    label.setStyle("-fx-font-size: 20px;");
+
+    Button dealButton = new Button("Deal new hand");
+    dealButton.setOnAction(e -> {
+      HandOfCards newHand = new HandOfCards(deck.dealHand());
+      label.setText(newHand.getHand().toString());
+    });
+
+    Scene scene = new Scene(new VBox(label, dealButton), 600, 400);
+
     primaryStage.setTitle("Card Game");
+    primaryStage.setScene(scene);
     primaryStage.show();
   }
 
