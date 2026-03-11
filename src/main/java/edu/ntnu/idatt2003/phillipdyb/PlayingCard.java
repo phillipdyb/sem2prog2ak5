a5dba2cdbce1b0e569c57334ab8a6f3e9354a1a5
Phillip Dyb-Sandnes <phillipdyb@hotmail.com>
2026-03-11 14:24:26 +0100

    Add PlayingCard constructor, get and toString

---
+++ src/main/java/edu/ntnu/idatt2003/phillipdyb/PlayingCard.java
@@ -0,0 +1,25 @@
+package edu.ntnu.idatt2003.phillipdyb;
+
+public class PlayingCard {
+  private final char suit;
+  private final int value;
+
+  public PlayingCard(char suit, int value) {
+    this.suit = suit;
+    this.value = value;
+  }
+
+  public char getSuit() {
+    return suit;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public String toString() {
+    return "" + suit + value;
+  }
+
+
+}
