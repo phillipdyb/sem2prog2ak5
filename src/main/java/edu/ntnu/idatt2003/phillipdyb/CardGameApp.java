31875d425e632476baeafaf6f3533516360ff682
Phillip Dyb-Sandnes <phillipdyb@hotmail.com>
2026-03-11 14:17:31 +0100

    Made 'mvn javafx:run' work

---
+++ src/main/java/edu/ntnu/idatt2003/phillipdyb/CardGameApp.java
@@ -0,0 +1,18 @@
+package edu.ntnu.idatt2003.phillipdyb;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CardGameApp extends Application {
+
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("Card Game");
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+}
