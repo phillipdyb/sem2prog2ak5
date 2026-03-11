63365757b5ce2fa9c0b1483f1d6fe723d34c5cea
Phillip Dyb-Sandnes <phillipdyb@hotmail.com>
2026-03-11 18:28:39 +0100

    Add error handling to junit test

--- src/test/java/DeckOfCardsTest.java
+++ src/test/java/DeckOfCardsTest.java
@@ -1,6 +1,7 @@
 import edu.ntnu.idatt2003.phillipdyb.DeckOfCards;
 import edu.ntnu.idatt2003.phillipdyb.PlayingCard;
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,7 +11,6 @@ class DeckOfCardsTest {
   @Test
   void dealHandReturnsCorrectNumberOfCards() {
     DeckOfCards deck = new DeckOfCards();
-
     List<PlayingCard> hand = deck.dealHand(5);
 
     assertEquals(5, hand.size());
@@ -19,7 +19,6 @@ class DeckOfCardsTest {
   @Test
   void dealtCardsHaveValidValues() {
     DeckOfCards deck = new DeckOfCards();
-
     List<PlayingCard> hand = deck.dealHand(10);
 
     for (PlayingCard card : hand) {
@@ -28,19 +27,28 @@ class DeckOfCardsTest {
   }
 
   @Test
-  void dealtCardsHaveValidSuits() {
+  void dealHandThrowsExceptionIfTooFewCardsRequested() {
     DeckOfCards deck = new DeckOfCards();
 
-    List<PlayingCard> hand = deck.dealHand(10);
-
-    for (PlayingCard card : hand) {
-      char suit = card.getSuit();
-      assertTrue(
-          suit == 'H' ||
-              suit == 'D' ||
-              suit == 'C' ||
-              suit == 'S'
-      );
-    }
+    assertThrows(IllegalArgumentException.class,
+        () -> deck.dealHand(0));
   }
+
+  @Test
+  void dealHandThrowsExceptionIfTooManyCardsRequested() {
+    DeckOfCards deck = new DeckOfCards();
+
+    assertThrows(IllegalArgumentException.class,
+        () -> deck.dealHand(53));
+  }
+
+  @Test
+  void dealtCardsShouldBeUnique() {
+    DeckOfCards deck = new DeckOfCards();
+
+    List<PlayingCard> hand = deck.dealHand(20);
+
+    assertEquals(hand.size(), hand.stream().distinct().count());
+  }
+
 }
